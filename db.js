ceaf5757799d8b42dfecf767989f5c40b36f9ab2
Jason Williams <jwilliamstap@gmail.com>
2020-11-14 23:44:51 -0500

    reformatted

--- db.js
+++ db.js
@@ -1,109 +1,101 @@
 const Sequelize = require('sequelize');
-const {STRING,DATE,UUID,UUIDV4} = Sequelize;
+const { STRING, DATE, UUID, UUIDV4 } = Sequelize;
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/reservations_db')
 
-const Tables = db.define('tables',{
-    table_name: {
-        type:STRING(20),
-        allowNull:false,
-        unique:true
-    }
+const Tables = db.define('tables', {
+  table_name: {
+    type: STRING(20),
+    allowNull: false,
+    unique: true
+  }
 })
 
-const People = db.define('people',{
-    id:{
-        type:UUID,
-        primaryKey:true,
-        defaultValue:UUIDV4
-    },
-    first_name:{
-        type:STRING(20),
-        allowNull:false,
-        unique:true
-    }
-})
-
-// const Waiters = db.define('waiter',{
-//     id:{
-//         type:UUID,
-//         primaryKey:true,
-//         defaultValue:UUIDV4
-//     },
-//     first_name:{
-//         type:STRING(20),
-//         allowNull:false,
-//         unique:true
-//     }
-// })
-
-const Reservations = db.define('reservations',{
-    startTime:{
-        type:DATE,
-        allowNull:false,
-    }
+const People = db.define('people', {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  first_name: {
+    type: STRING(20),
+    allowNull: false,
+    unique: true
+  }
 })
 
 
-Reservations.belongsTo(People,{as: 'reservedBy'});
-People.hasMany(Reservations,{foreignKey:'reservedById'});
+const Reservations = db.define('reservations', {
+  reserveTime: {
+    type: DATE,
+    allowNull: false
+  }
+})
 
-Reservations.belongsTo(Tables,{as: 'table'});
-Tables.hasMany(Reservations,{foreignKey:'tableId'});
 
-People.belongsTo(People,{ as: 'waitedBy'});
-People.hasMany(People,{ foreignKey: 'waitedById'})
+Reservations.belongsTo(People,{ as: 'reservedBy' });
+People.hasMany(Reservations,{ foreignKey:'reservedById' });
+
+Reservations.belongsTo(Tables,{ as: 'table' });
+Tables.hasMany(Reservations,{ foreignKey:'tableId' });
+
+People.belongsTo(People,{ as: 'waitedBy' });
+People.hasMany(People,{ foreignKey: 'waitedById' })
+
 
 const syncAndSeed = async() => {
-    await db.sync({force:true});
+  await db.sync({ force:true });
 
-    const [moe,lucy,larry, curley, joe, floe, zoe]=await Promise.all(
-        ['moe','lucy','larry', 'curley', 'joe', 'floe', 'zoe'].map(first_name => People.create({first_name}))
-        );
+  const [moe,lucy,larry, curley, joe, floe, zoe]=await Promise.all(
+    ['moe','lucy','larry', 'curley', 'joe', 'floe', 'zoe'].map(first_name => People.create({ first_name }))
+    );
 
-    const [table1, table2, table3]=await Promise.all(
-        ['table 1', 'table 2', 'table 3'].map(table_name => Tables.create({table_name}))
-        );
+  const [table1, table2, table3]=await Promise.all(
+    ['table 1', 'table 2', 'table 3'].map(table_name => Tables.create({ table_name }))
+    );
 
-    const [reservation1, reservation2, reservation3, reservation4]=await Promise.all([
-        Reservations.create({startTime:'2020-03-14 10:57:12.72-05'}),
-        Reservations.create({startTime:'2020-04-14 18:57:12.72-05'}),
-        Reservations.create({startTime:'2020-11-01 10:57:12.72-05'}),
-        Reservations.create({startTime:'2020-10-30 09:57:12.72-05'})
-    ])
-
-
-    reservation1.reservedById=moe.id;
-    reservation1.tableId=table1.id;
-    moe.waitedById=joe.id;
-    reservation2.reservedById=larry.id;
-    reservation2.tableId=table2.id;
-    larry.waitedById=floe.id;
-    reservation3.reservedById=lucy.id;
-    reservation3.tableId=table3.id;
-    lucy.waitedById=zoe.id;
-    reservation4.reservedById=curley.id;
-    reservation4.tableId=table1.id;
-    curley.waitedById=zoe.id;
+  const [reservation1, reservation2, reservation3, reservation4]=await Promise.all([
+    Reservations.create({ reserveTime: '2020-11-14 10:57:12.72-05' }),
+    Reservations.create({ reserveTime: '2020-11-15 18:57:12.72-05' }),
+    Reservations.create({ reserveTime: '2020-11-15 10:57:12.72-05' }),
+    Reservations.create({ reserveTime: '2020-11-21 09:57:12.72-05' })
+  ])
 
 
-    await Promise.all([
-        reservation1.save(),
-        reservation2.save(),
-        reservation3.save(),
-        reservation4.save(),
-        moe.save(),
-        larry.save(),
-        lucy.save(),
-        curley.save()
-    ])
+  reservation1.reservedById=moe.id;
+  reservation1.tableId=table1.id;
+  moe.waitedById=joe.id;
+
+  reservation2.reservedById=larry.id;
+  reservation2.tableId=table2.id;
+  larry.waitedById=floe.id;
+
+  reservation3.reservedById=lucy.id;
+  reservation3.tableId=table3.id;
+  lucy.waitedById=zoe.id;
+
+  reservation4.reservedById=curley.id;
+  reservation4.tableId=table1.id;
+  curley.waitedById=zoe.id;
+
+
+  await Promise.all([
+    reservation1.save(),
+    reservation2.save(),
+    reservation3.save(),
+    reservation4.save(),
+    moe.save(),
+    larry.save(),
+    lucy.save(),
+    curley.save()
+  ])
 }
 
-module.exports={
-    db,
-    syncAndSeed,
-    model:{
-        Tables,
-        People,
-        Reservations
-    }
+module.exports = {
+  db,
+  syncAndSeed,
+  model:{
+    Tables,
+    People,
+    Reservations
+  }
 }
--- server.js
+++ server.js
@@ -1,82 +1,78 @@
-const {db,syncAndSeed,model:{Tables, People, Reservations}} = require('./db');
+const { db, syncAndSeed, model:{ Tables, People, Reservations } } = require('./db');
 const express = require('express');
-const path = require('path')
-//const { urlencoded } = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended:false }));
 app.use(require('method-override')('_method'));
-//app.use('/',express.static(path.join(__dirname)));
 app.use('/public', express.static(path.join(__dirname, 'public'))); 
 
 
-app.get('/', (req, res)=> {
-    res.redirect('/reservations')
+app.get('/', (req, res) => {
+  res.redirect('/reservations')
 })
 
 
-app.get('/customers', async(req, res, next)=>{
-    try{
-        const people = await People.findAll({
-            include: [
-                Reservations
-            ],
-            order: ['id']
-        })
-        res.send(people);
-    }
-    catch(ex){
-        next(ex);
-    }
+app.get('/customers', async(req, res, next) =>{
+  try{
+    const people = await People.findAll({
+      include: [
+        Reservations
+      ],
+      order: ['id']
+    })
+    res.send(people);
+  }
+  catch(ex){
+    next(ex);
+  }
 })
 
 
-app.get('/tables', async(req, res, next)=> {
-    try{
-        const tables = await Tables.findAll({
-            include: [
-                Reservations
-            ]
-        })
-        res.send(tables);
-    }
-    
-    catch(ex) {
-        next(ex);
-    }
+app.get('/tables', async(req, res, next) => {
+  try{
+    const tables = await Tables.findAll({
+      include: [
+        Reservations
+      ]
+    })
+    res.send(tables);
+  }
+  catch(ex) {
+    next(ex);
+  }
 })
 
 
-app.get('/reservations', async(req, res, next)=> {
-    try{
-        const reservations = await Reservations.findAll({
-            include: [{
-                model: Tables,
-                as: 'table'
-            },
-            {
-                model: People,
-                as: 'reservedBy'
-            }],
-            order: ['id']
-        })
-        res.send(reservations)
-    }
-    catch(ex) {
-        next(ex)
-    }
+app.get('/reservations', async(req, res, next) => {
+  try{
+    const reservations = await Reservations.findAll({
+      include: [{
+        model: Tables,
+        as: 'table'
+      },
+      {
+        model: People,
+        as: 'reservedBy'
+      }],
+      order: ['id']
+    })
+    res.send(reservations)
+  }
+  catch(ex) {
+    next(ex)
+  }
 })
 
 
 const init = async() => {
-    try{
-        await syncAndSeed();
-        console.log('READY');
-        const port = process.env.PORT || 3000
-        app.listen(port,()=>{console.log(`Listening on Port: ${port}`)})
-    }catch(error){
-        console.log(error);
-    }
+  try{
+    await syncAndSeed();
+    const port = process.env.PORT || 3000
+    app.listen(port,()=>{console.log(`Listening on Port: ${port}`)})
+  }catch(error){
+    console.log(error);
+  }
 }
 
 init()
